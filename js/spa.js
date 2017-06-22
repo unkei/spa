f34961492c135ec58cba75a2fb05b1d66caae1f2
unkei <unkei@yahoo.co.jp>
2017-06-22 18:39:14 +0900

    ex. 3-11. page 94

--- js/spa.js
+++ js/spa.js
@@ -3,21 +3,17 @@
  */
 
 
-/*  jslint browser          : true, continue    : true,
-    devel   : true, indent  : 2, maxerr         : 50,
-    newcap  : true, nomen   : true, plusplus    : true,
-    regexp  : true, sloppy  : true, vars        : false,
-    white   : true
+/*jslint browser          : true, continue    : true,
+  devel   : true, indent  : 2, maxerr         : 50,
+  newcap  : true, nomen   : true, plusplus    : true,
+  regexp  : true, sloppy  : true, vars        : false,
+  white   : true
 */
-/* global $, spa:true */
+/*global $, spa:true */
 
 var spa = (function(){
     var initModule = function ( $container ) {
-        $container.html(
-                '<h1 style="display:inline-block; margin:25px;">'
-                + 'hello world!'
-                + '</h1>'
-                );
+        spa.shell.initModule( $container );
     };
     return { initModule : initModule };
 }());
